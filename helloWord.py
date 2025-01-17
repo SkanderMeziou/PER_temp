0999ddc21b4e66ded1951a1c130b0382362dc5ff
Skander Meziou <95022243+SkanderMeziou@users.noreply.github.com>
2025-01-17 14:06:48 +0100

    delete useless comments

--- helloWord.py
+++ helloWord.py
@@ -1,6 +1,4 @@
-# Ouvre un fichier en mode écriture (ou le crée s'il n'existe pas)
 with open("hello_world.txt", "w") as file:
-    # Écrit le message dans le fichier
     file.write("Hello World")
 
 print("Fichier 'hello_world.txt' créé avec succès.")
